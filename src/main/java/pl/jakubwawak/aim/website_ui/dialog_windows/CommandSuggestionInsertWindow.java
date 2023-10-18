08c3252090cc09e5b5149e6bab47d5b8f9fc775c
Jakub Wawak <34348257+wjakew@users.noreply.github.com>
2023-10-18 11:10:18 +0200

    aim171023REV01

--- src/main/java/pl/jakubwawak/aim/website_ui/dialog_windows/CommandSuggestionInsertWindow.java
+++ src/main/java/pl/jakubwawak/aim/website_ui/dialog_windows/CommandSuggestionInsertWindow.java
@@ -11,11 +11,15 @@ import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.H6;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import lombok.extern.java.Log;
 import pl.jakubwawak.aim.website_ui.views.TerminalView;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Window for logging user to the app
@@ -63,13 +67,15 @@ public class CommandSuggestionInsertWindow {
             property1_field.setWidth("100%");
             property1_field.setPlaceholder("value");
         }
-        if (!property2.isEmpty()){
-            property2_field = new TextField(property2);
-            property2_field.setWidth("100%");
-            property2_field.setPlaceholder("value");
+        if ( property2 != null ){
+            if (!property2.isEmpty()){
+                property2_field = new TextField(property2);
+                property2_field.setWidth("100%");
+                property2_field.setPlaceholder("value");
+            }
         }
 
-        createcommand_button = new Button("Create Command", VaadinIcon.ARROW_RIGHT.create());
+        createcommand_button = new Button("Create Command", VaadinIcon.ARROW_RIGHT.create(),this::createcommandbutton_action);
         createcommand_button.addThemeVariants(ButtonVariant.LUMO_CONTRAST,ButtonVariant.LUMO_PRIMARY);
         createcommand_button.setWidth("100%");
 
@@ -87,7 +93,7 @@ public class CommandSuggestionInsertWindow {
         if ( !property1.isEmpty() ){
             main_layout.add(property1_field);
         }
-        if ( !property2.isEmpty() ){
+        if ( property2 != null){
             main_layout.add(property2_field);
         }
 
@@ -106,10 +112,74 @@ public class CommandSuggestionInsertWindow {
     }
 
     /**
+     * Function for replacing words in sentences
+     * @param source
+     * @param orgin
+     * @param replace
+     * @return
+     */
+    String replaceWords(String source, String orgin, String replace){
+        String s= source;
+        s=s+" ";
+        String newsen="";String temp="";
+        for(int i=0;i<s.length();i++)
+        {
+            char x=s.charAt(i);
+            if(x!=' ')
+            {
+                temp=temp+x;
+            }
+            else
+            {
+                if(orgin.equals(temp))
+                {
+                    newsen=newsen+ replace;
+                }
+                else
+                {
+                    newsen=newsen+temp;
+                }
+                newsen=newsen+" ";
+                temp="";
+            }
+        }
+        return newsen;
+    }
+
+    /**
      * createcommand_button action
      * @param ex
      */
     private void createcommandbutton_action(ClickEvent ex){
+        if (property1_field != null && property2_field != null){
+            if (!property1_field.getValue().isEmpty() && !property2_field.getValue().isEmpty()){
+                if ( property1 != null ){
+                    selectedCommand = replaceWords(selectedCommand,property1,property1_field.getValue());
+                }
+                if ( property2 !=null ){
+                    selectedCommand = replaceWords(selectedCommand,property2,property2_field.getValue());
+                }
+                terminalView.terminal_field.setValue(selectedCommand);
+                main_dialog.close();
+                Notification.show("Command created, set!");
+            }
+            else{
+                Notification.show("Wrong user input, fields are empty!");
+            }
+        }
+        else{
+            if (!property1_field.getValue().isEmpty()){
+                if ( property1 != null ){
+                    selectedCommand = replaceWords(selectedCommand,property1,property1_field.getValue());
+                }
+                terminalView.terminal_field.setValue(selectedCommand);
+                main_dialog.close();
+                Notification.show("Command created, set!");
+            }
+            else{
+                Notification.show("Wrong user input, fields are empty!");
+            }
+        }
 
     }
 }
